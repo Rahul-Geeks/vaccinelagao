a02f822794e66af82061b5d1de5fa62062be5d9a
Rahul Chouhan <rahul@xamplay.com>
2021-05-13 16:59:38 +0530

    Vaccine info for Hbad + inform twitter & telegram

---
+++ server.js
@@ -0,0 +1,112 @@
+const request = require("request");
+const TwitterBot = require("twitter");
+const moment = require("moment");
+const express = require("express");
+const TelegramBot = require('node-telegram-bot-api');
+
+let config = require("./config");
+
+// Setting twitter configuration
+let twitter = new TwitterBot(config.twitter);
+
+// Setting telegram configuration
+let telegramToken = config.telegram;
+let telegram = new TelegramBot(telegramToken, { polling: true });
+
+let app = express();
+
+// Get the information if vaccine doses are available
+let getVaccineDoses = () => {
+    let date = moment(new Date());
+    let today = date.format('DD-MM-YYYY');      // Today's date
+
+    // Make a request to CoWin server
+    request({
+        uri: "https://cdn-api.co-vin.in/api/v2/appointment/sessions/calendarByPin",
+        method: "GET",
+        qs: {
+            pincode: "461001",
+            date: today
+        }
+    }, (error, res, body) => {
+        if (error)
+            console.log("ERROR:", error);
+
+        else if (body && body != "Unauthenticated access!") {
+            let activeSessions = [];
+
+            // Get all sessions with age 18+ and available vaccines
+            JSON.parse(body).centers.forEach(center => {
+                let filSessions = center.sessions.filter(session => {
+                    // if (session.min_age_limit == 18 && session.available_capacity == 0) {
+                    if (session.min_age_limit == 18 && session.available_capacity > 0) {
+                        session.center = center.name;
+                        return session;
+                    }
+                });
+
+                if (filSessions[0])
+                    activeSessions = activeSessions.concat(filSessions);
+            });
+            console.log(activeSessions);
+
+            // If atleast one session found
+            if (activeSessions[0]) {
+                console.log("YES, FOUND AN ACTIVE SESSION");
+                console.log(activeSessions[0].session_id);
+
+                // Inform twitter and telegram users about vaccine availibility
+                activeSessions.forEach(s => {
+                    informTwitter(s.available_capacity, s.center, s.date);
+                    informTelegram(s.available_capacity, s.center, s.date);
+                });
+            }
+            else {
+                console.log("NOT AVAILABLE", date.format('LT'));
+            }
+        }
+        else
+            console.log("SOME PROBLEM OCCURRED", error, body);
+    });
+}
+
+// Get vaccine info in every few seconds
+setInterval(getVaccineDoses, 10000 * 3);
+// setInterval(getVaccineDoses, 3000);
+
+// Informing twitter about vaccine
+let informTwitter = (capacity, centerName, date) => {
+    twitter.post('statuses/update', {
+        status: `Vaccination slots alert (18-44 age) for Hoshangabad, M.P 461001.
+        Center: ${centerName}
+        Slots available: ${capacity}
+        Date: ${date}
+        #CovidIndia #CovidVaccineIndia #Hoshangabad`
+    }, (error, tweet, response) => {
+        if (error)
+            console.log("TWEET ERROR", error);
+        else
+            console.log("TWEETED");
+    });
+}
+
+let bookAppointment = () => {
+    request({
+        uri: 'https://cdn-api.co-vin.in/api/v2/appointment/schedule',
+        method: 'POST',
+
+    })
+}
+
+// Informing telegram about vaccine
+let informTelegram = (capacity, centerName, date) => {
+    telegram.sendMessage('-583948379', `Vaccination slots alert (18-44 age) for Hoshangabad, M.P 461001.
+    Center: ${centerName}
+    Slots available: ${capacity}
+    Date: ${date}`);
+}
+
+// Run server
+app.listen(5000, () => {
+    console.log("App running on port 5000!")
+})
